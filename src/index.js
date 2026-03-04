bd2391a6f754ef1431b07a34dbf8a90a022dc98a
SunnyDev7 <sunnydev4589@gmail.com>
2026-03-04 14:35:09 +0100

    boiler plate code created

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+const app = express();
+
+const PORT = 8000 ?? process.env.PORT;
+
+try {
+  await mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log(`MongoDB Connected Successfully!`);
+  });
+} catch (error) {
+  console.log(error);
+}
+
+app.get("/", (req, res) => {
+  res.send("Welcome to practice server");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server up on ${PORT}`);
+});
